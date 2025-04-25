9b2567c85b98135a49fb9227b45c18039021e554
Mauricio <buffamauricio@gmail.com>
2025-04-25 18:41:53 -0300

    product list ui

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["placehold.co"],
+  },
 };
 
 export default withPayload(nextConfig);
