8d5e9e3128181c3747a31faa15256be47685ccf1
Mauricio <buffamauricio@gmail.com>
2025-04-26 15:37:47 -0300

    tenant pages

---
+++ src/modules/tenants/ui/components/footer.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+
+const poppins = Poppins({
+  weight: ["700"],
+  subsets: ["latin"],
+});
+
+export const Footer = () => {
+  return (
+    <footer className="border-t font-medium bg-white">
+      <div className="max-w-(--breakpoint-xl) mx-auto flex items-center h-full px-4 py-6 lg:px-12 gap-2">
+        <p className="text-xl">Powered by</p>
+        <Link
+          href="/"
+          className={cn("text-2xl font-semibold", poppins.className)}
+        >
+          funroad
+        </Link>
+      </div>
+    </footer>
+  );
+};
